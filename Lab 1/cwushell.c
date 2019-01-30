c399cfc85fb49aaa0f5e3efc3e3e3dca9569c6c6
MattSHarker <mattsharker@gmail.com>
2019-01-30 09:09:58 -0800

    Original Upload
    Created a program which runs from command line that sorts numbers using a version of Merge Sort which forks everytime it has to split an array.

---
+++ Lab 1/cwushell.c
@@ -0,0 +1,245 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <ctype.h>
+
+#define clear() printf("\033[H\033[J") 
+
+char prompt[] = "cwushell>";
+
+void init()
+{
+    clear();
+    printf("***************************************");
+    printf("\n***************************************");
+    printf("\n************** CWU SHELL **************");
+    printf("\n*********** by Matt Harker ************");
+    printf("\n***************************************");
+    printf("\n***************************************");
+    
+    printf("\n\nFor a list of commands enter \"manual\"\n\n");
+    
+}
+
+// displays a list of commands
+void displayHelp()
+{
+    printf("\n ******** HELP ********");
+    printf("\nList of commands:");
+    
+    printf("\nprompt <new_prompt>");
+    printf("\n    modifies the current shell prompt to <new_prompt>");
+    
+    printf("\ncpuinfo -switch");
+    printf("\n    available switches: -c, -t, -n");
+    
+    printf("\nmeminfo -switch");
+    printf("\n    availabe switches: -t, -u, -c");
+    
+    printf("\nmanual");
+    printf("\n    displays a list of commands available in this shell");
+    
+    printf("\nFor more info on specific commands, use -h or -help as a swicth\n");
+}
+
+// shows a help message for the prompt command
+void displayPromptHelp()
+{
+    printf("In order to use the prompt command, enter it as follows: \n");
+    printf("    prompt <new_prompt>\n");
+    printf("Enter what you want the new prompt to be where it says <new_prompt>\n");    
+}
+
+// changes the shell's prompt
+void changePrompt(char* str)
+{
+    strcpy(prompt, str);
+}
+
+// displays a help message for the cpuinfo command
+void displayCpuHelp()
+{
+    printf("cpuinfo has the following switches:\n");
+    printf("    -c: displays the clock speed\n");
+    printf("    -t: displays the cpu manufacturer\n");
+    printf("    -n: displays the number of cores\n");
+}
+
+// displays the speed of the cpu clock
+void displayCpuClock()
+{
+    system("lscpu | grep 'MHz'");
+}
+
+// displays the brand of the cpu
+void displayCpuType()
+{
+    system("cat /proc/cpuinfo | grep 'vendor' | uniq");
+}
+
+// prints the number of cores
+void displayCpuCores()
+{
+    // write is used as system() is not buffered
+    write(1, "Number of cores: ", 17);  
+    system("nproc --all");
+}
+
+
+// displays a help message for the meminfo command
+void displayMemHelp()
+{
+    printf("meminfo has the following switches:\n");
+    printf("    -t: displays the amount of available ram\n");
+    printf("    -u: displays the amount of used ram\n");
+    printf("    -c: displays the size of the L2 cache\n");
+}
+
+// displays the amount of available ram
+void displayAvailableRam()
+{
+    system("cat /proc/meminfo | grep 'MemAvailable'");
+}
+
+// displays the amount of used ram
+void displayUsedRam()
+{
+    system("cat /proc/meminfo | grep 'Active:'");
+}
+
+// displays the size of the L2 chache/core in bytes
+void displayCacheSize()
+{
+    write(1, "Size of L2 cache (bytes): ", 26);
+    long memFree = system("getconf LEVEL2_CACHE_SIZE");
+}
+
+int startsWith(const char* pre, const char *str)
+{
+    return strncmp(pre, str, strlen(pre)) == 0;
+}
+
+int endsWithAngle(const char* str)
+{
+    str = strrchr(str, '>');
+    
+    if(str != NULL)
+        return (strcmp(str, ">"));
+    
+    return(-1);
+}
+
+int main(int argc, char** argv)
+{
+    init();
+    
+    char inputString[100];   // the input to be executed
+
+    while (1)
+    {
+        printf("\n%s ", prompt);
+        fgets(inputString, sizeof(inputString), stdin);
+        
+        // displays the general help message
+        if (strcmp(inputString, "manual\n") == 0 || strcmp(inputString, "help\n") == 0)
+        {
+            displayHelp();
+        }
+        
+        
+        // cpu info
+        else if(strcmp(inputString, "cpuinfo -c\n") == 0)
+        {
+            displayCpuClock();
+        }
+        else if(strcmp(inputString, "cpuinfo -t\n") == 0)
+        {
+            displayCpuType();
+        }
+        else if(strcmp(inputString, "cpuinfo -n\n") == 0)
+        {
+            displayCpuCores();
+        }
+        else if(strcmp(inputString, "cpuinfo\n")  == 0 || strcmp(inputString, "cpuinfo -h\n") == 0
+                                          || strcmp(inputString, "cpuinfo -help\n") == 0)
+        {
+            displayCpuHelp();
+        }
+          
+            
+        // meminfo
+        else if(strcmp(inputString, "meminfo\n") == 0 || strcmp(inputString, "meminfo -h\n") == 0
+                                               || strcmp(inputString, "meminfo -help\n") == 0)
+        {
+            displayMemHelp();
+        }
+        else if (strcmp(inputString, "meminfo -t\n") == 0)
+        {
+            displayAvailableRam();
+        }    
+        else if (strcmp(inputString, "meminfo -u\n") == 0)
+        {
+            displayUsedRam();
+        }
+        else if(strcmp(inputString, "meminfo -c\n") == 0)
+        {
+            displayCacheSize();
+        }
+        
+        
+        // The Prompt command
+        else if (strcmp(inputString, "prompt\n") != 0 && strcmp(inputString, "prompt -h\n") != 0
+                                                      && strcmp(inputString, "prompt -help\n") != 0
+                                                      && startsWith("prompt", inputString))
+        {
+            char* token;
+            token = strtok(inputString, " \n");
+            token = strtok(NULL, " \n");
+            
+            strcpy(prompt, token);
+            
+        }
+        else if (strcmp(inputString, "prompt\n") == 0 || strcmp(inputString, "prompt -h\n") == 0
+                                                   || strcmp(inputString, "prompt -help\n") == 0)
+        {
+            displayPromptHelp();
+        }
+        
+        // the exit command
+        else if (strcmp(inputString, "exit\n") != 0 && startsWith("exit", inputString))
+        {
+            char* token;
+            
+            token = strtok(inputString, " \n");
+            token = strtok(NULL, " \n");
+            
+            if (isdigit(token) == 1)
+            {
+                int extCode = atoi(token);
+                return extCode;
+            }
+            else
+                return 0;
+            
+        }
+        else if (strcmp(inputString, "exit\n") == 0)
+        {
+        
+            return 0;
+        }
+        
+        
+        
+        // to handle anything else
+        else
+        {
+        
+            system(inputString);
+        }
+    }
+}
+
+
---
+++ Lab 2/Fork_Sort.c
@@ -0,0 +1,238 @@
+
+/****************************
+		Matthew Harker
+		CS 470, Lab 2
+ ****************************/
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <string.h>
+
+#define BUFFER_SIZE 1024
+#define ARR_SIZE 128
+
+
+// reads in a file to be used later
+int readFile(char buff[], char* name)
+{
+	FILE *fptr;
+	fptr = fopen(name, "r");
+	
+	if (fptr == NULL)
+	{
+		printf("File was not found\n");
+		return 1;
+	}
+	
+	fscanf(fptr, "%[^\n]", buff);
+	printf("Original string: %s\n", buff);
+	fclose(fptr);
+	
+	return 0;
+}
+
+
+// prints an array to the console
+void printArr(int arr[], int left, int right)
+{
+	for(int i = left; i < right; i++)
+		printf("%d ", arr[i]);
+	
+	printf("\n");
+}
+
+
+int merge(int arr[], int left, int mid, int right)
+{
+	// sizes of subarrays
+	int lSize = mid - left + 1;
+	int rSize = right - mid;
+	
+	
+	// copy data to temp subarrays
+	int lArr[lSize], rArr[rSize];
+	
+	for (int i = 0; i < lSize; i++)
+		lArr[i] = arr[left+i];
+	
+	for (int j = 0; j < rSize; j++)
+		rArr[j] = arr[mid + 1 + j];
+
+	
+	// print initial status
+	printf("Process ID: %u\n", getpid());
+	printf("Subarray 1: ");
+	printArr(lArr, 0, lSize);
+	printf("Subarray 2: ");
+	printArr(rArr, 0, rSize);
+	
+	
+	// merge temp arrays back 
+	int i = 0;		// for left subarray
+	int j = 0;		// for right subarray
+	int k = left;	// for merged subarray
+	
+	while (i < lSize && j < rSize)
+	{		
+		if (lArr[i] <= rArr[j])
+		{
+			arr[k] = lArr[i];
+			i++;
+		}
+		else
+		{
+			arr[k] = rArr[j];
+			j++;
+		}
+		
+		k++;
+	}
+	
+	// ensures all elements of lArr are copied over
+	while (i < lSize)
+	{
+		arr[k] = lArr[i];
+		++i;
+		++k;
+	}
+	
+	// ensures all elements of rArr are copied over
+	while (j < rSize)
+	{
+		arr[k] = rArr[j];
+		++j;
+		++k;
+	}
+	
+	
+	// print merged status
+	printf("Merged arrays: ");
+	printArr(arr, left, right + 1);
+	printf("\n");
+}
+
+
+void mergeSort(int arr[], int left, int right)
+{
+	// only if there is more that 1 element
+    if (left < right)
+    {
+	    int status;
+	    pid_t lchild, rchild;
+	    
+    	int mid = (left + right)/2;
+    	
+    	lchild = fork();
+    	
+    	if (lchild < 0)
+    	{
+    		perror("fork");
+    		exit(1);
+    	}
+    	if (lchild == 0)
+    	{
+    		mergeSort(arr, left, mid);
+    		exit(0);
+    	}
+    	else
+    	{
+	    	rchild = fork();
+	    	
+	    	if (rchild < 0)
+	    	{
+	    		perror("fork");
+	    		exit(1);
+	    	}
+	    	if (rchild == 0)
+	    	{
+    			mergeSort(arr, mid+1, right);
+    			exit(0);
+	    	}	
+    	}
+    	
+    	// wait for the forks to catch up
+    	waitpid(lchild, &status, 0);
+    	waitpid(rchild, &status, 0);
+    	
+    	
+    	// merge the subarrays
+    	merge(arr, left, mid, right);
+    }
+}
+
+
+int main(int argc, char* argv[])
+{
+	// read in the file
+    char buff[BUFFER_SIZE];
+    
+    if(readFile(buff, argv[1]) == 1)
+    	return 1;
+    
+    
+    // create an array in shared memory
+    int *arr;
+    int shmid;
+    key_t key = IPC_PRIVATE;
+    
+	if ((shmid = shmget(key, ARR_SIZE, IPC_CREAT | 0666)) < 0)
+	{
+		perror("shmget");
+		exit(1);
+	}
+	
+    if ((arr = shmat(shmid, NULL, 0)) == (int *)-1)
+    {
+    	perror("shmat");
+    	exit(1);
+    }
+    
+    
+    // tokenize the input into the array
+    int arrSize = 0;
+    
+    char *dlm = argv[2];	// the delimiter that determines what seperates the numbers
+	char *tok;
+	
+    tok = strtok(buff, dlm);
+    
+    while (tok != NULL)
+    {
+    	arr[arrSize++] = atoi(tok);
+    	tok = strtok(NULL, dlm);
+    }
+    
+    
+    // sort the array
+    printf("Original array: ");
+    printArr(arr, 0, arrSize);
+    printf("\n");
+    
+    mergeSort(arr, 0, arrSize-1);
+    
+    printf("Sorted array: ");
+    printArr(arr, 0, arrSize);
+    
+    
+    // finish dealing with the shared memory array
+    if (shmdt(arr) == -1)
+    {
+    	perror("shmdt");
+    	exit(1);
+    }
+    
+    if (shmctl(shmid, IPC_RMID, NULL) == -1)
+    {
+    	perror("shmctl");
+    	exit(1);
+    }
+    
+    return 0;
+}
+
+
